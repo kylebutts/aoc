a31e72431534e0f3bb234ad8195fd2ebe96c5893
Kyle Butts <buttskyle96@gmail.com>
2022-12-05 07:11:24 -0700

    2022 Day 5 Solutions

---
+++ 2022/5/a.jl
@@ -0,0 +1,56 @@
+input = readlines("2022/5/input.txt") 
+
+# Parse input
+dirs = match.(
+  r"move ([0-9]+) from ([0-9]+) to ([0-9]+)",
+  input
+)
+setup = input[dirs .== nothing]
+dirs = dirs[dirs .!= nothing]
+
+# Parse setup
+rownums_idx = occursin.(r"[0-9]", setup) |> 
+  (x -> findall(x)[1])
+rownums = setup[rownums_idx] |> 
+  (x -> split(x, " ")) |>
+  (x -> x[x .!= ""])|> 
+  (x -> parse.(Int, x)) |> 
+  (x -> maximum(x))
+
+stacks = [Char[] for _ in 1:rownums]
+
+regex = [raw"[\[ ]([A-Z ])[\] ]" for _ in 1:rownums]
+regex = Regex(join(regex, " "))
+matches = match.(
+  regex, 
+  reverse(setup[1:rownums_idx-1])
+)
+
+for row in matches
+  row = collect(row)
+  for i in eachindex(row)
+    el = row[i]
+    el = convert(Char, el[1])
+    if (el !== nothing) & (el != ' ')
+      push!(stacks[i], el)
+    end
+  end
+end
+
+
+# Apply instructions
+# row = dirs[1]
+
+for row in dirs
+  N_move = parse(Int, row[1])
+  from = parse(Int, row[2])
+  to = parse(Int, row[3])
+
+  for i in 1:N_move 
+    el = pop!(stacks[from])
+    push!(stacks[to], el)
+  end
+end
+
+join(map(x -> x[end], stacks), "")
+
